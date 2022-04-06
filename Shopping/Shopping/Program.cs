32afc6652929406099a78dd515e9bfd49bd2f2db
JUAN SEBASTIAN CARDONA SERNA <jcardser@gmail.com>
2022-04-05 23:36:28 -0500

    Initial Class

--- Shopping/Shopping/Program.cs
+++ Shopping/Shopping/Program.cs
@@ -1,15 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Shopping.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddControllersWithViews();
+
+//Configurar la inyección de independencias, cada que necesite el datacontext se la va a tirar con el ctor
+builder.Services.AddDbContext<DataContext>(o =>
+{
+    o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
