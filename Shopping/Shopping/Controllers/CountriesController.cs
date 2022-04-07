192b87c3fc52e633302ade7ec5c7470b19bd0960
JUAN SEBASTIAN CARDONA SERNA <jcardser@gmail.com>
2022-04-06 22:59:55 -0500

    Second class

--- Shopping/Shopping/Controllers/CountriesController.cs
+++ Shopping/Shopping/Controllers/CountriesController.cs
@@ -13,20 +13,20 @@ namespace Shopping.Controllers
 {
     public class CountriesController : Controller
     {
-        private readonly DataContext _context;
-
+        private readonly DataContext _context; 
+ 
         public CountriesController(DataContext context)
         {
             _context = context;
         }
 
-        // GET: Countries
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             return View(await _context.Countries.ToListAsync());
         }
 
-        // GET: Countries/Details/5
+        [HttpGet]
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -44,18 +44,15 @@ namespace Shopping.Controllers
             return View(country);
         }
 
-        // GET: Countries/Create
+        [HttpGet]
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Countries/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name")] Country country)
+        public async Task<IActionResult> Create(Country country)
         {
             if (ModelState.IsValid)
             {
